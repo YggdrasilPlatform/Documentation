3e4e747762a96b8230196dc163553cc995a28f73
WerWolv <werwolv98@gmail.com>
2021-06-29 13:27:51 +0200

    config: Added back a minimal Beta banner

--- conf.py
+++ conf.py
@@ -82,8 +82,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv', 'README.md','.gi
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+is_beta = True
+
 html_theme = 'furo'
-html_title = "Yggdrasil Developer Guide"
+html_title = "Yggdrasil Developer Guide &nbsp;&nbsp; " + ("[ BETA ]" if is_beta else "")
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
